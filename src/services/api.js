3f7c0871ab476e43ccf23520448b5644db6a2b11
Ahadraza <ahadraza4533@gmail.com>
2026-02-06 12:33:29 +0530

    done

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,9 @@
+// const API_BASE_URL =
+//   process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+  process.env.REACT_APP_API_URL ||
+  "https://appointment-backend-e5z9.onrender.com/api";
 
 /**
  * Get stored auth token
