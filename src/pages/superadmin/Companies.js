7aff476644f93b6dba41376e3686fdbe0bf82ecb
Ahadraza <ahadraza4533@gmail.com>
2026-02-07 11:49:35 +0530

    done

--- src/pages/superadmin/Companies.js
+++ src/pages/superadmin/Companies.js
@@ -203,9 +203,9 @@ const Companies = () => {
 
                     <td>
                       <div className="sa-actions-cell">
-                        <a
-                          href={`/superadmin/companies/${company._id}`}
-                          className="sa-action-btn"
+                        <Link
+                          to={`/superadmin/companies/${company._id}`}
+                          className="sa-action-btn view"
                           title="View Details"
                         >
                           <svg
@@ -213,11 +213,15 @@ const Companies = () => {
                             fill="none"
                             stroke="currentColor"
                             strokeWidth="2"
+                            width="18"
+                            height="18"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                           >
-                           <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-                           <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+                            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                            <circle cx="12" cy="12" r="3" />
                           </svg>
-                        </a>
+                        </Link>
                         <button
                           className="sa-action-btn delete"
                           title="Delete Company"
