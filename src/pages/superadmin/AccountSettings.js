801a01cbe60d81d428a0a62f473c40993a810368
Ahadraza <ahadraza4533@gmail.com>
2026-02-10 11:45:48 +0530

    done

--- src/pages/superadmin/AccountSettings.js
+++ src/pages/superadmin/AccountSettings.js
@@ -115,6 +115,8 @@ const AccountSettings = () => {
       setLoading(false);
     }
   };
+  console.log("Submitting profile:", profile);
+
 
   return (
     <div className="sa-dashboard-container">
