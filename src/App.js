8efda877ef375b6cbe4e8bcc3ec35de6ef6abea6
Ahadraza <ahadraza4533@gmail.com>
2026-02-11 13:24:16 +0530

    done

--- src/App.js
+++ src/App.js
@@ -85,6 +85,10 @@ function App() {
             />
             <Route path="my-appointments" element={<MyAppointments />} />
             <Route path="profile" element={<CustomerProfile />} />
+
+            <Route path="payment" element={<Payment />} />
+            <Route path="payment-success" element={<PaymentSuccess />} />
+            <Route path="payment-failed" element={<PaymentFailed />} />
           </Route>
 
           <Route path="about" element={<About />} />
